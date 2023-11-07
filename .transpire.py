7bd473bc9907c72aba6901d8436646aecf0effa5
Victor Shi <jieteng@berkeley.edu>
2023-11-06 20:43:13 -0800

    added shelve

--- .transpire.py
+++ .transpire.py
@@ -1,22 +1,23 @@
 from pathlib import Path
 
 from transpire.resources import Deployment, Ingress, Service
+from transpire.internal import surgery
 from transpire.types import Image
 from transpire.utils import get_image_tag
 
-NAME = "phpmyadmin"
+name = "phpmyadmin"
 
 
 def objects():
     dep = Deployment(
-        name=NAME,
-        image=get_image_tag(NAME),
+        name="phpmyadmin",
+        image=get_image_tag("phpmyadmin"),
         ports=[8000],
     )
 
     svc = Service(
-        name=NAME,
-        selector=dep.get_selector(NAME),
+        name="phpmyadmin",
+        selector=dep.get_selector(),
         port_on_pod=8000,
         port_on_svc=80,
     )
@@ -27,10 +28,15 @@ def objects():
         path_prefix="/",
     )
 
-    yield dep.build()
+    dns = {"dnsPolicy": "ClusterFirst", "dnsConfig": {"searches": "ocf.berkeley.edu"}}
+
+    # add dnsPolicy and dnsConfig to deployment
+    yield surgery.shelve(
+        dep.build(), ["spec", "template", "spec"], dns, create_parents=True
+    )
     yield svc.build()
     yield ing.build()
 
 
 def images():
-    yield Image(name=NAME, path=Path("/"))
+    yield Image(name=name, path=Path("/"))
